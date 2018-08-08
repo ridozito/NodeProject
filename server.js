20b6aa4af166114bad0b379b9988972c089e7867
Hung Nguyen <hungnvcntt91@gmail.com>
2018-08-08 16:50:34 +0700

    Init structure

---
+++ server.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello Tedu'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
