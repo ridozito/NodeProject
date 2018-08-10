c7c7b8ed792bab1ecb7477e65cffe3fe1c25162f
Hung Nguyen <hungnvcntt91@gmail.com>
2018-08-10 16:47:42 +0700

    Change theme

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 "use strict";
 const express = require('express');
 const app = express();
+const path = require('path');
 var pug = require('pug');
 app.set("view engine","pug");
-app.set("views","./components/common");
-
+ app.set("views",[path.join(__dirname,"components/views/common"),path.join(__dirname,"components/views/index")]);
+app.use(express.static(__dirname+'/components/views/public'));
 
 app.get('/', (req, res) => res.render("header.pug"));
-
+app.get('/index', (req, res) => res.render("index.pug"));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
