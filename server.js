e09e2277a03a4148630c76873f442c894ca3ec9b
Hung Nguyen <hungnvcntt91@gmail.com>
2018-08-10 11:37:05 +0700

    Demo

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 "use strict";
 const express = require('express');
-const app = express()
+const app = express();
+var pug = require('pug');
+app.set("view engine","pug");
+app.set("views","./components/common");
 
-app.get('/', (req, res) => res.send('Hello Tedu'));
+
+app.get('/', (req, res) => res.render("header.pug"));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
